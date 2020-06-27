2f38acbd891fceb4569b643e9424c7d22007a635
Алексей <xapohob@mail.ru>
2020-06-27 19:51:07 +0400

    Добавьте файлы проекта.

---
+++ zachet1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zachet1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        link:
+            string stringVariable = Console.ReadLine();
+            int max = 0, len = 0;
+            for (int i = 0; i < stringVariable.Length; i++)
+            {
+                if (stringVariable[i] == ' ')
+                {
+                    len++;
+                    if (len > max)
+                        max = len;
+                }
+                else
+                    len = 0;
+            }
+            Console.WriteLine(max);
+            goto link;
+        }
+    }
+}
